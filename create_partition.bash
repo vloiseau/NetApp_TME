ae8a8eea2bdd3804faa65bb39f82cc45e6567873
faizabidi <fabidi89@vt.edu>
2017-09-26 15:14:19 -0400

    Added distinction between sda and the rest since sda is already in use and using GPT.

--- create_partition.bash
+++ create_partition.bash
@@ -9,13 +9,20 @@ fi
 
 set -ex
 
+# g=create a new GPT partition table. We need this because MBR can't support >2TB
 # n=add a new partition
 # Press enter for default, which would basically be the next available number
 # Press enter for default First Sector
 # Press enter for default Last Sector
 # Press t to change a partition's system id
-# Press enter to select the default Partition number
+# Press enter to select the default Partition number. This won't be needed for any except sda, which is being used by root, swap, etc.
 # Press 15 for selectig LVM
 # Press w for writing it to the disk
-echo -e "n\n\n\n\nt\n\n15\nw" | sudo fdisk /dev/$PARTITION_NAME
+if [ ! $PARTITION_NAME == "sda" ]; then
+    echo -e "g\nn\n\n\n\nt\n15\nw" | sudo fdisk /dev/$PARTITION_NAME
+elif [ $PARTITION_NAME == "sda" ]; then
+    echo -e "n\n\n\n\nt\n\n15\nw" 
+    # Need to reboot since sda was not unmounted and in use
+    sudo reboot
+fi
 
