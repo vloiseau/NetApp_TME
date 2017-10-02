5fc2644e4e38aaa1ee1cc240103f801cc38eccde
faizabidi <fabidi89@vt.edu>
2017-10-02 15:00:57 -0400

    Example run of hibench.

---
+++ hibench_wordcount.bash
@@ -0,0 +1,8 @@
+#/bin/bash
+
+# Create the data first
+/home/faiz89/git/HiBench/bin/workloads/micro/wordcount/prepare/prepare.sh
+
+# Run the test
+/home/faiz89/git/HiBench/bin/workloads/micro/wordcount/hadoop/run.sh
+
