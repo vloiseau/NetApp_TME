d2eed2a91c618d6a8d534b1dd2e4d4f2856fc7f1
faizabidi <fabidi89@vt.edu>
2017-10-06 10:13:26 -0400

    Moved all ganglia related stuff here.

---
+++ ganglia/install_ganglia.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# On the server
+sudo yum install ganglia rrdtool ganglia-gmetad ganglia-gmond ganglia-web 
+
+# Make change to the /etc/ganglia/gmetad.conf
+# Also make changes to /etc/ganglia/gmond.conf
+# See here for reference - https://www.digitalocean.com/community/tutorials/introduction-to-ganglia-on-ubuntu-14-04
+
+# Restart ganglia
+sudo service httpd restart
+sudo service gmetad restart
+sudo service gmond restart
+
+# On the worker nodes
+sudo yum install ganglia-gmond -y
+# Copy the same gmond file from the server to this host
+# Restart gmond
+sudo service gmond restart
