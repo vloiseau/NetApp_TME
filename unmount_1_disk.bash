c63ce0cf64675350008bde582955edf0adf1df13
faizabidi <fabidi89@vt.edu>
2017-10-06 16:57:12 -0400

    Unmount 1 disk from each slave.

---
+++ unmount_1_disk.bash
@@ -0,0 +1,28 @@
+#/bin/bash
+
+
+# Install lsof if not already there
+CHECK_LSOF="sudo yum list installed lsof >/dev/null 2>&1"
+INSTALL_LSOF="sudo yum install lsof -y"
+KILL_PROCESS="sudo lsof | grep /mnt/sdd1/ | awk '{print \$2}' | xargs kill"
+
+for ((i=136; i<=146; i+=2)); do
+    ssh -t -o ConnectTimeout=5 $i << HERE
+    # Commands to run on the remote host
+    if ! eval $CHECK_LSOF; then
+        echo "Installing lsof"
+        eval $INSTALL_LSOF
+    fi
+    # Find out the process using the mount point and kill it
+    #sudo lsof | grep /mnt/sdd1/ | awk '{print \$2}' | xargs kill
+    #eval $KILL_PROCESS >/dev/null 2>&1
+    # I tried using -f of umount, didn't work. The I started  checking the processes using that filesystem using lsof.
+    # This seemed to work in the sense that I could unmount the filesystem but this also apparently killed the datanode daemon
+    # Finally, found out a "lazy" way to unmount a filesystem using the -l flag
+    # Now unmount it
+    if sudo umount -l /mnt/sdd1; then
+        echo "Successfully unmounted /mnt/sdd1"
+    fi
+HERE
+done
+
