61f3f5f5ab736071687b9f6897ed91b217f95f37
faizabidi <fabidi89@vt.edu>
2017-10-10 11:36:36 -0400

    Minor changes.

--- mount_1_disk.bash
+++ mount_1_disk.bash
@@ -1,13 +1,15 @@
 #/bin/bash
 
 # Unmount the filesystem
-for ((i=136; i<=146; i+=2)); do
+for ((i=136; i<=138; i+=2)); do
     if ssh -t $i \
-            'sudo mount /dev/hdfs_data/sdd1-lvm /mnt/sdd1'; then
+            'sudo mount /dev/hdfs_data/sdd1-lvm /mnt/sdd1 && \
+            sudo mount /dev/hdfs_data/sde1-lvm /mnt/sde1 && \
+            sudo mount /dev/hdfs_data/sdf1-lvm /mnt/sdf1'; then
             #sudo mount /dev/hdfs_data/sde1-lvm /mnt/sde1 && \
             #sudo mount /dev/hdfs_data/sdf1-lvm /mnt/sdf1 && \
             #sudo mount /dev/hdfs_data/sdg1-lvm /mnt/sdg1'
-        echo "Successfully mounted /mnt/sdd1"
+        echo "Successfully mounted /mnt/sdd1, /mnt/sde1, /mnt/sdf1 on worker-$i"
     fi
 done
 
--- unmount_1_disk.bash
+++ unmount_1_disk.bash
@@ -6,9 +6,10 @@ CHECK_LSOF="sudo yum list installed lsof >/dev/null 2>&1"
 INSTALL_LSOF="sudo yum install lsof -y"
 KILL_PROCESS="sudo lsof | grep /mnt/sdd1/ | awk '{print \$2}' | xargs kill"
 
-for ((i=136; i<=146; i+=2)); do
+for ((i=136; i<=138; i+=2)); do
     ssh -t -o ConnectTimeout=5 $i << HERE
     # Commands to run on the remote host
+    # Check if lsof is installed
     if ! eval $CHECK_LSOF; then
         echo "Installing lsof"
         eval $INSTALL_LSOF
@@ -20,8 +21,8 @@ for ((i=136; i<=146; i+=2)); do
     # This seemed to work in the sense that I could unmount the filesystem but this also apparently killed the datanode daemon
     # Finally, found out a "lazy" way to unmount a filesystem using the -l flag
     # Now unmount it
-    if sudo umount -l /mnt/sdd1; then
-        echo "Successfully unmounted /mnt/sdd1"
+    if sudo umount -l /mnt/sdd1 && sudo umount -l /mnt/sde1 && sudo umount -l /mnt/sdf1; then
+        echo "Successfully unmounted /mnt/sdd1, /mnt/sde1, and /mnt/sdf1 from worker-$i"
     fi
 HERE
 done
