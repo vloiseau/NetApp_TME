616706acae888126fb09a79e85ded513066f8284
faizabidi <fabidi89@vt.edu>
2017-12-02 17:29:14 -0500

    Decreased sleep time to 1 minute, removed Bayes, and running each job thrice.

--- run_hibench_jobs.bash
+++ run_hibench_jobs.bash
@@ -6,19 +6,31 @@ set -ex
 # Run the job
 echo "Starting wc job at" $(date +"%T")
 /home/faiz89/git/HiBench/bin/workloads/micro/wordcount/hadoop/run.sh \
-	> /home/faiz89/wc_output.txt
+	> /home/faiz89/wc_output1.txt
+sleep 1m
+/home/faiz89/git/HiBench/bin/workloads/micro/wordcount/hadoop/run.sh \
+	> /home/faiz89/wc_output2.txt
+sleep 1m
+/home/faiz89/git/HiBench/bin/workloads/micro/wordcount/hadoop/run.sh \
+	> /home/faiz89/wc_output3.txt
 echo "Finished wc job at" $(date +"%T")
-# Sleep for 5 minutes
-sleep 5m
+# Sleep for 1 minute
+sleep 1m
 
 # 2. Terrasort
 # Run the job
 echo "Starting ts job at" $(date +"%T")
 /home/faiz89/git/HiBench/bin/workloads/micro/terasort/hadoop/run.sh \
-	> /home/faiz89/ts_output.txt
+	> /home/faiz89/ts_output1.txt
+sleep 1m
+/home/faiz89/git/HiBench/bin/workloads/micro/terasort/hadoop/run.sh \
+	> /home/faiz89/ts_output2.txt
+sleep 1m
+/home/faiz89/git/HiBench/bin/workloads/micro/terasort/hadoop/run.sh \
+	> /home/faiz89/ts_output3.txt
 echo "Finished ts job at" $(date +"%T")
-# Sleep for 5 minutes
-sleep 5m
+# Sleep for 1 minute
+sleep 1m
 
 # Use bigdata for DFSIO tests
 #sed -i 's/gigantic/bigdata/g' /home/faiz89/git/HiBench/conf/hibench.conf
@@ -28,17 +40,25 @@ sleep 5m
 # Run the job
 echo "Starting dfsio read job at" $(date +"%T")
 /home/faiz89/git/HiBench/bin/workloads/micro/dfsioe/hadoop/run_read.sh
+sleep 1m
+/home/faiz89/git/HiBench/bin/workloads/micro/dfsioe/hadoop/run_read.sh
+sleep 1m
+/home/faiz89/git/HiBench/bin/workloads/micro/dfsioe/hadoop/run_read.sh
 echo "Finished dfsio read job at" $(date +"%T")
-# Sleep for 5 minutes
-sleep 5m
+# Sleep for 1 minute
+sleep 1m
 
 # 4. DFSIOE write
 # Run the job
 echo "Starting dfsio write job at" $(date +"%T")
 /home/faiz89/git/HiBench/bin/workloads/micro/dfsioe/hadoop/run_write.sh
+sleep 1m
+/home/faiz89/git/HiBench/bin/workloads/micro/dfsioe/hadoop/run_write.sh
+sleep 1m
+/home/faiz89/git/HiBench/bin/workloads/micro/dfsioe/hadoop/run_write.sh
 echo "Finished dfsio write job at" $(date +"%T")
-# Sleep for 5 minutes
-sleep 5m
+# Sleep for 1 minute
+sleep 1m
 
 # Move back to gigantic data
 #sed -i 's/bigdata/gigantic/g' /home/faiz89/git/HiBench/conf/hibench.conf
@@ -47,48 +67,79 @@ sleep 5m
 # Run the job
 echo "Starting sql aggregation job at" $(date +"%T")
 /home/faiz89/git/HiBench/bin/workloads/sql/aggregation/hadoop/run.sh \
-	> /home/faiz89/sql_aggre_output.txt
+	> /home/faiz89/sql_aggre_output1.txt
+sleep 1m
+/home/faiz89/git/HiBench/bin/workloads/sql/aggregation/hadoop/run.sh \
+	> /home/faiz89/sql_aggre_output2.txt
+sleep 1m
+/home/faiz89/git/HiBench/bin/workloads/sql/aggregation/hadoop/run.sh \
+	> /home/faiz89/sql_aggre_output3.txt
 echo "Finished sql aggregation job at" $(date +"%T")
-# Sleep for 5 minutes
-sleep 5m
+# Sleep for 1 minute
+sleep 1m
 
 # 6. SQL join
 # Run the job
 echo "Starting sql join job at" $(date +"%T")
 /home/faiz89/git/HiBench/bin/workloads/sql/join/hadoop/run.sh \
-	> /home/faiz89/sql_join_output.txt
+	> /home/faiz89/sql_join_output1.txt
+sleep 1m
+/home/faiz89/git/HiBench/bin/workloads/sql/join/hadoop/run.sh \
+	> /home/faiz89/sql_join_output2.txt
+sleep 1m
+/home/faiz89/git/HiBench/bin/workloads/sql/join/hadoop/run.sh \
+	> /home/faiz89/sql_join_output3.txt
 echo "Finished sql join job at" $(date +"%T")
-# Sleep for 5 minutes
-sleep 5m
+# Sleep for 1 minute
+sleep 1m
 
 # 7. SQL scan
 # Run the job
 echo "Starting sql scan job at" $(date +"%T")
 /home/faiz89/git/HiBench/bin/workloads/sql/scan/hadoop/run.sh \
-	> /home/faiz89/sql_scan_output.txt
+	> /home/faiz89/sql_scan_output1.txt
+sleep 1m
+/home/faiz89/git/HiBench/bin/workloads/sql/scan/hadoop/run.sh \
+	> /home/faiz89/sql_scan_output2.txt
+sleep 1m
+/home/faiz89/git/HiBench/bin/workloads/sql/scan/hadoop/run.sh \
+	> /home/faiz89/sql_scan_output3.txt
 echo "Finished sql scan job at" $(date +"%T")
-# Sleep for 5 minutes
-sleep 5m
+# Sleep for 1 minute
+sleep 1m
 
 # 8. Pagerank
 # Run the job
 echo "Starting pagerank job at" $(date +"%T")
 /home/faiz89/git/HiBench/bin/workloads/websearch/pagerank/hadoop/run.sh \
-	> /home/faiz89/pagerank_output.txt
+	> /home/faiz89/pagerank_output1.txt
+sleep 1m
+/home/faiz89/git/HiBench/bin/workloads/websearch/pagerank/hadoop/run.sh \
+	> /home/faiz89/pagerank_output2.txt
+sleep 1m
+/home/faiz89/git/HiBench/bin/workloads/websearch/pagerank/hadoop/run.sh \
+	> /home/faiz89/pagerank_output3.txt
 echo "Finished pagerank job at" $(date +"%T")
-# Sleep for 5 minutes
-sleep 5m
+# Sleep for 1 minute
+sleep 1m
 
 # ML jobs take a lot more time than others. Hence, running at the end
 # 9. ML Kmeans
 # Run the job
 echo "Starting ml kmeans job at" $(date +"%T")
 /home/faiz89/git/HiBench/bin/workloads/ml/kmeans/hadoop/run.sh \
-	> /home/faiz89/ml_kmeans.txt
+	> /home/faiz89/ml_kmeans1.txt
+sleep 1m
+/home/faiz89/git/HiBench/bin/workloads/ml/kmeans/hadoop/run.sh \
+	> /home/faiz89/ml_kmeans2.txt
+sleep 1m
+/home/faiz89/git/HiBench/bin/workloads/ml/kmeans/hadoop/run.sh \
+	> /home/faiz89/ml_kmeans3.txt
 echo "Finished ml kmeans job at" $(date +"%T")
-# Sleep for 5 minutes
-#sleep 5m
+# Sleep for 1 minute
+#sleep 1m
 
+# Not running Bayes since Bigdata was taking forever to complete
 # 10. ML Bayes
 # Run the job
 #echo "Starting ml bayes job at" $(date +"%T")
