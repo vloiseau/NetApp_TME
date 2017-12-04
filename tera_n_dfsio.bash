a8aa6f792e585ad7805f29e3679d8134674c85e9
faizabidi <fabidi89@vt.edu>
2017-12-04 16:39:34 -0500

    Modified a little.

--- tera_n_dfsio.bash
+++ tera_n_dfsio.bash
@@ -1,46 +1,62 @@
 #!/bin/bash
 
 # Teragen
-#RUN 1
-# Remove the data first
+# RUN 1
+# Remove the old data first
 hadoop fs -rm -r  /HiBench/Dfsioe/terasort-input
 # Run the job
 /usr/hdp/2.6.2.0-205/hadoop/bin/hadoop --config /usr/hdp/2.6.2.0-205/hadoop/etc/hadoop jar /usr/hdp/2.6.2.0-205/hadoop/../hadoop-mapreduce/hadoop-mapreduce-examples.jar teragen -D mapreduce.job.maps=304 -D mapreduce.job.reduces=304 10995116277 /HiBench/Dfsioe/terasort-input
-
-#RUN 2
-# Remove the data first
+sleep 1m
+# RUN 2
+# Remove the old data first
 hadoop fs -rm -r  /HiBench/Dfsioe/terasort-input
 # Run the job
 /usr/hdp/2.6.2.0-205/hadoop/bin/hadoop --config /usr/hdp/2.6.2.0-205/hadoop/etc/hadoop jar /usr/hdp/2.6.2.0-205/hadoop/../hadoop-mapreduce/hadoop-mapreduce-examples.jar teragen -D mapreduce.job.maps=304 -D mapreduce.job.reduces=304 10995116277 /HiBench/Dfsioe/terasort-input
-
-#RUN 3
-# Remove the data first
+sleep 1m
+# RUN 3
+# Remove the old data first
 hadoop fs -rm -r  /HiBench/Dfsioe/terasort-input
 # Run the job
 /usr/hdp/2.6.2.0-205/hadoop/bin/hadoop --config /usr/hdp/2.6.2.0-205/hadoop/etc/hadoop jar /usr/hdp/2.6.2.0-205/hadoop/../hadoop-mapreduce/hadoop-mapreduce-examples.jar teragen -D mapreduce.job.maps=304 -D mapreduce.job.reduces=304 10995116277 /HiBench/Dfsioe/terasort-input
+sleep 1m
 #############################################################################################
 # Terasort
-#RUN 1
+# RUN 1
 # Remove the old output
 hadoop fs -rm -r /HiBench/Dfsioe/terasort-output
 # Run the job
 /usr/hdp/2.6.2.0-205/hadoop/bin/hadoop --config /usr/hdp/2.6.2.0-205/hadoop/etc/hadoop jar /usr/hdp/2.6.2.0-205/hadoop/../hadoop-mapreduce/hadoop-mapreduce-examples.jar terasort -D mapreduce.job.reduces=304 /HiBench/Dfsioe/terasort-input /HiBench/Dfsioe/terasort-output
-
-#RUN 1
+sleep 1m
+# RUN 2
 # Remove the old output
 hadoop fs -rm -r /HiBench/Dfsioe/terasort-output
 # Run the job
 /usr/hdp/2.6.2.0-205/hadoop/bin/hadoop --config /usr/hdp/2.6.2.0-205/hadoop/etc/hadoop jar /usr/hdp/2.6.2.0-205/hadoop/../hadoop-mapreduce/hadoop-mapreduce-examples.jar terasort -D mapreduce.job.reduces=304 /HiBench/Dfsioe/terasort-input /HiBench/Dfsioe/terasort-output
-
-#RUN 1
+sleep 1m
+# RUN 3
 # Remove the old output
 hadoop fs -rm -r /HiBench/Dfsioe/terasort-output
 # Run the job
 /usr/hdp/2.6.2.0-205/hadoop/bin/hadoop --config /usr/hdp/2.6.2.0-205/hadoop/etc/hadoop jar /usr/hdp/2.6.2.0-205/hadoop/../hadoop-mapreduce/hadoop-mapreduce-examples.jar terasort -D mapreduce.job.reduces=304 /HiBench/Dfsioe/terasort-input /HiBench/Dfsioe/terasort-output
+sleep 1m
 #############################################################################################
 # DFSIO WRITE
+# RUN 1
 hadoop jar /usr/hdp/2.6.2.0-205/hadoop-mapreduce/hadoop-mapreduce-client-jobclient-tests.jar TestDFSIO -write -nrFiles 256 -fileSize 4096
+sleep 1m
+# RUN 2
+hadoop jar /usr/hdp/2.6.2.0-205/hadoop-mapreduce/hadoop-mapreduce-client-jobclient-tests.jar TestDFSIO -write -nrFiles 256 -fileSize 4096
+sleep 1m
+# RUN 3
+hadoop jar /usr/hdp/2.6.2.0-205/hadoop-mapreduce/hadoop-mapreduce-client-jobclient-tests.jar TestDFSIO -write -nrFiles 256 -fileSize 4096
+sleep 1m
 #############################################################################################
 # DFSIO READ
+# RUN 1
 hadoop jar /usr/hdp/2.6.2.0-205/hadoop-mapreduce/hadoop-mapreduce-client-jobclient-tests.jar TestDFSIO -read -nrFiles 256 -fileSize 4096
-
+sleep 1m
+# RUN 2
+hadoop jar /usr/hdp/2.6.2.0-205/hadoop-mapreduce/hadoop-mapreduce-client-jobclient-tests.jar TestDFSIO -read -nrFiles 256 -fileSize 4096
+sleep 1m
+# RUN 3
+hadoop jar /usr/hdp/2.6.2.0-205/hadoop-mapreduce/hadoop-mapreduce-client-jobclient-tests.jar TestDFSIO -read -nrFiles 256 -fileSize 4096
