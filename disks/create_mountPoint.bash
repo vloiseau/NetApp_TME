c0e80c65763f9e405d3892de5bfd754e7c79320f
faizabidi <fabidi89@vt.edu>
2017-12-11 12:11:57 -0500

    Some changes to accommodate different mount points for different disks.

--- disks/create_mountPoint.bash
+++ disks/create_mountPoint.bash
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-MOUNT_POINT_LOCATION=$1
-LV=$2
+DISK=$1
+MOUNT_POINT_LOCATION==$2
 
 if [ $# -ne 2 ]; then
-    echo "Give the path of the mount point and the logical volume you are trying to point." 
-    echo "For example, /mnt/sda4 and sda4-lvm etc."
+    echo "Give the path of the disk trying to be mounted and the mount point." 
+    echo "For example, sdm1 and disk1 etc."
     exit 1
 fi
 
 set -ex
 
 # Create the mount folder first
-sudo mkdir $MOUNT_POINT_LOCATION
+#sudo mkdir $MOUNT_POINT_LOCATION
 
 # Mount the XFS file system to this mount point
-if sudo mount /dev/hdfs_data/$LV $MOUNT_POINT_LOCATION; then
-    echo "Successfully mounted $LV to $MOUNT_POINT_LOCATION"
+if sudo mount /dev/$DISK /mnt/$MOUNT_POINT_LOCATION; then
+    echo "Successfully mounted $DISK to $MOUNT_POINT_LOCATION"
     echo "Try "df -h" to confirm."
 fi
 
 # Change the ownership of /mnt
-sudo chown -R faiz89:faiz89 /mnt
+#sudo chown -R faiz89:faiz89 /mnt
 
-echo "Remember to format the namenode afterwards."
+#echo "Remember to format the namenode afterwards."
 
